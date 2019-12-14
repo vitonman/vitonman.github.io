1e7d6fe8f2645ed9c2875d61c5f4e8e4e067265a
vitodon <45338513+vitonman@users.noreply.github.com>
2019-12-14 21:10:31 +0200

    finish for now

--- js/radar.js
+++ js/radar.js
@@ -3,10 +3,10 @@ var ctxR = document.getElementById("radarChart").getContext('2d');
 var myRadarChart = new Chart(ctxR, {
 type: 'radar',
 data: {
-labels: ["Java", "Android", "Python", "PHP", "JS", "CSS", "HTML"],
+labels: ["Java", "Android", "Python", "PHP", "JS", "CSS", "HTML", "SQL"],
 datasets: [{
 label: "My current skills",
-data: [67, 59, 55, 60, 50, 59, 75],
+data: [67, 59, 55, 60, 50, 59, 75, 64],
 backgroundColor: [
 'rgba(105, 0, 132, .2)',
 ],
@@ -17,7 +17,7 @@ borderWidth: 2
 },
 {
 label: "My future preference",
-data: [90, 75, 73, 75, 82, 72, 75],
+data: [90, 75, 73, 75, 82, 72, 85, 78],
 backgroundColor: [
 'rgba(0, 250, 220, .2)',
 ],
